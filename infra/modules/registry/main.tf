bb5a4a02c4d265eecd3993442127b2b0ec468987
valuedriven <junilson@hotmail.com>
2026-03-16 20:27:16 -0300

    ci: Pin Terraform AWS provider version and enhance workflow commands for automation.

--- infra/modules/registry/main.tf
+++ infra/modules/registry/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "frontend" {
   name                 = "${var.project_name}-frontend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -15,6 +16,7 @@ resource "aws_ecr_repository" "frontend" {
 resource "aws_ecr_repository" "backend" {
   name                 = "${var.project_name}-backend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
