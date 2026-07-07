68b4bbc1a2c4c5e742b0a4fd7772d9a45799762a
valuedriven <junilson@hotmail.com>
2026-07-07 15:33:10 -0300

    chore: update db_name default value to remove hyphens

--- infra/environments/dev/variables.tf
+++ infra/environments/dev/variables.tf
@@ -19,7 +19,7 @@ variable "environment" {
 variable "db_name" {
   description = "Name of the database"
   type        = string
-  default     = "devai-dev-db"
+  default     = "devaidevdb"
 }
 
 variable "db_username" {
